d4c079e4cd0197f07e9806c654f35194fe933c78
Avdonina Lada <144753760+avd0nina@users.noreply.github.com>
2025-09-17 11:36:51 +0700

    Update DefaultMessageSender.java

--- networks and telecommunications/lab1/CopyDetector/src/main/java/org.example/DefaultMessageSender.java
+++ networks and telecommunications/lab1/CopyDetector/src/main/java/org.example/DefaultMessageSender.java
@@ -5,30 +5,38 @@ import java.io.IOException;
 import java.io.ObjectOutputStream;
 import java.net.DatagramPacket;
 import java.net.InetAddress;
+import java.net.InetSocketAddress;
 import java.net.MulticastSocket;
 
 public class DefaultMessageSender implements MessageSender {
-    private final static int kByte = 1024;
     private final MulticastSocket multicastSocket;
-    private final InetAddress inetAddress;
+    private final InetAddress multicastGroupAddress;
     private final int port;
+    private final String scopeId;
 
-    public DefaultMessageSender(MulticastSocket multicastSocket, InetAddress inetAddress, int port) {
+    public DefaultMessageSender(MulticastSocket multicastSocket, InetAddress multicastGroupAddress, int port, String scopeId) {
         this.multicastSocket = multicastSocket;
-        this.inetAddress = inetAddress;
+        this.multicastGroupAddress = multicastGroupAddress;
         this.port = port;
+        this.scopeId = scopeId;
     }
 
     @Override
-    public void sendMessage(final Message message) throws IOException {
-        final ByteArrayOutputStream data = new ByteArrayOutputStream(kByte);
-        final ObjectOutputStream objectOutputStream = new ObjectOutputStream(data);
+    public void sendMessage(Message message) throws IOException {
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        ObjectOutputStream objectOutputStream = new ObjectOutputStream(byteArrayOutputStream);
         objectOutputStream.writeObject(message);
-        final DatagramPacket datagramPacket = new DatagramPacket(
-                data.toByteArray(),
-                data.size(),
-                inetAddress,
-                port);
+        objectOutputStream.flush();
+        byte[] buffer = byteArrayOutputStream.toByteArray();
+
+        InetSocketAddress destination;
+        if (multicastGroupAddress instanceof java.net.Inet6Address && scopeId != null) {
+            destination = new InetSocketAddress(InetAddress.getByName(multicastGroupAddress.getHostAddress() + "%" + scopeId), port);
+        } else {
+            destination = new InetSocketAddress(multicastGroupAddress, port);
+        }
+
+        DatagramPacket datagramPacket = new DatagramPacket(buffer, buffer.length, destination);
         multicastSocket.send(datagramPacket);
     }
 }
