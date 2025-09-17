7dd3772da7b4a2cc398d17ee6f207f92b895a53f
Avdonina Lada <144753760+avd0nina@users.noreply.github.com>
2025-09-17 07:52:01 +0700

    Create ByeMessage.java

---
+++ networks and telecommunications/lab1/CopyDetector/src/main/java/org.example/messages/ByeMessage.java
@@ -0,0 +1,18 @@
+package org.example.messages;
+
+import org.example.*;
+
+import java.util.List;
+
+public class ByeMessage extends Message {
+    @Override
+    public void handle(List<AppCopy> appCopies, MessageSender sender, MessageReceiver receiver) {
+        for (final var it : appCopies) {
+            if (it.pid() == getPid()) {
+                appCopies.remove(it);
+                break;
+            }
+        }
+        MessageUtils.printAppCopies(appCopies);
+    }
+}
