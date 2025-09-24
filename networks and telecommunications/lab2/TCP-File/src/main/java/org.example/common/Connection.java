3f3fa05b6bff5f45304fe0f3df2db7858fa08e52
Avdonina Lada <144753760+avd0nina@users.noreply.github.com>
2025-09-24 12:50:41 +0700

    Create Connection.java

---
+++ networks and telecommunications/lab2/TCP-File/src/main/java/org.example/common/Connection.java
@@ -0,0 +1,15 @@
+package org.example.common;
+
+public interface Connection {
+    void send(byte[] bytes);
+
+    void sendWithSize(byte[] bytes);
+
+    void sendWithSize(String message);
+
+    void setRawMode(long bytesCountToRead, int bufferSize);
+
+    boolean isClosed();
+
+    void close();
+}
