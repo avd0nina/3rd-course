a2eac72b494f3e464a38ed0da9b536c70ee7aab0
Avdonina Lada <144753760+avd0nina@users.noreply.github.com>
2025-09-24 12:51:00 +0700

    Create ConnectionListener.java

---
+++ networks and telecommunications/lab2/TCP-File/src/main/java/org.example/common/ConnectionListener.java
@@ -0,0 +1,11 @@
+package org.example.common;
+
+public interface ConnectionListener {
+    void onConnectionReady(Connection connection);
+
+    void onDisconnect(Connection connection);
+
+    void onBytesReceived(Connection connection, byte[] bytes);
+
+    void onException(Connection connection, Exception exception);
+}
