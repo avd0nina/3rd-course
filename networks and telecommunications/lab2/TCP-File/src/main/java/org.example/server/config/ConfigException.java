1b7665b1dd0749aaf0d58d684fb1b1f4973d0eb1
Avdonina Lada <144753760+avd0nina@users.noreply.github.com>
2025-09-24 12:53:32 +0700

    Create ConfigException.java

---
+++ networks and telecommunications/lab2/TCP-File/src/main/java/org.example/server/config/ConfigException.java
@@ -0,0 +1,15 @@
+package org.example.server.config;
+
+public class ConfigException extends Exception {
+    public ConfigException(String message) {
+        super(message);
+    }
+
+    public ConfigException(Throwable cause) {
+        super(cause);
+    }
+
+    public ConfigException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
