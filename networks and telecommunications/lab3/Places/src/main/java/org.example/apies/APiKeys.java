267eba9f162dd3c4ded851f191a2b4ef133bf0a9
Avdonina Lada <144753760+avd0nina@users.noreply.github.com>
2025-09-28 15:00:37 +0700

    Create APiKeys.java

---
+++ networks and telecommunications/lab3/Places/src/main/java/org.example/apies/APiKeys.java
@@ -0,0 +1,7 @@
+package org.example.apies;
+
+public class APiKeys {
+    public static final String locationApiKey = "YOUR_GRAPHHOPPER_API_KEY";
+    public static final String weatherApiKey = "YOUR_OPENWEATHER_API_KEY";
+    public static final String interestLocationApiKey = "YOUR_OPENTRIPMAP_API_KEY";
+}
