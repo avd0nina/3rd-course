444332f6f26a691f960282b3126dd71f67e83c9b
Avdonina Lada <144753760+avd0nina@users.noreply.github.com>
2025-10-13 11:10:37 +0700

    Create GameDisplayThread.java

---
+++ networks and telecommunications/lab4/SnakeOnlineGame/src/main/java/org.example/model/threads/GameDisplayThread.java
@@ -0,0 +1,30 @@
+package org.example.model.threads;
+
+import org.example.model.Model;
+import java.util.logging.Logger;
+
+public class GameDisplayThread extends Thread {
+    private static final Logger logger = Logger.getLogger(GameDisplayThread.class.getSimpleName());
+    private boolean isRunning = true;
+    private final Model model;
+
+    public GameDisplayThread(Model model) {
+        this.model = model;
+    }
+
+    @Override
+    public void run() {
+        while (isRunning) {
+            try {
+                sleep(1000);
+            } catch (InterruptedException e) {
+                logger.info("Thread interrupted");
+            }
+            model.showActiveGames();
+        }
+    }
+
+    public void setStopped() {
+        isRunning = false;
+    }
+}
