7cbc485abe297bdd19135deb4a9c9da6fc02a9dd
Avdonina Lada <144753760+avd0nina@users.noreply.github.com>
2025-10-16 01:10:40 +0700

    Update mythread.c

--- operation systems/lab1/lab1.6/mythread.c
+++ operation systems/lab1/lab1.6/mythread.c
@@ -15,6 +15,12 @@
 
 static int thread_num = 0;
 
+/**
+ * Функция запуска потока, вызываемая через clone.
+ * Выполняет переданную функцию потока и устанавливает флаг завершения.
+ * @param arg Указатель на структуру mythread_struct_t
+ * @return Всегда 0
+ */
 int mythread_startup(void *arg) {
     mythread_struct_t *mythread = (mythread_struct_t *) arg;
     mythread->retval = mythread->start_routine(mythread->arg);
@@ -22,6 +28,13 @@ int mythread_startup(void *arg) {
     return 0;
 }
 
+/**
+ * Выделяет память для стека потока с защитной страницей.
+ * Использует mmap для выделения памяти и mprotect для создания guard page.
+ * @param stack Указатель для хранения адреса стека
+ * @param size Размер стека (без учёта защитной страницы)
+ * @return 0 при успехе, -1 при ошибке
+ */
 int create_stack(void **stack, size_t size) {
     size_t total_size = size + GUARD_PAGE;
     *stack = mmap(NULL, total_size, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS | MAP_STACK, -1, 0);
@@ -38,6 +51,14 @@ int create_stack(void **stack, size_t size) {
     return 0;
 }
 
+/**
+ * Создаёт новый поток с помощью clone.
+ * Выделяет стек и структуру потока, инициализирует метаданные.
+ * @param thread Указатель для хранения дескриптора потока
+ * @param start_routine Функция, выполняемая в потоке
+ * @param arg Аргумент для функции потока
+ * @return 0 при успехе, -1 при ошибке
+ */
 int mythread_create(mythread_t *thread, start_routine_t start_routine, void *arg) {
     if (start_routine == NULL) {
         fprintf(stderr, "mythread_create: start_routine is NULL\n");
@@ -77,6 +98,13 @@ int mythread_create(mythread_t *thread, start_routine_t start_routine, void *arg
     return 0;
 }
 
+/**
+ * Ожидает завершения потока и освобождает его ресурсы.
+ * Использует активное ожидание через флаг exited, затем освобождает стек и структуру.
+ * @param thread Дескриптор потока
+ * @param retval Указатель для возвращаемого значения (может быть NULL)
+ * @return 0 при успехе, -1 при ошибке
+ */
 int mythread_join(mythread_t thread, void **retval) {
     mythread_struct_t *mythread = thread;
     while (!mythread->exited) {
@@ -88,6 +116,6 @@ int mythread_join(mythread_t thread, void **retval) {
     if (mythread->stack) {
         munmap(mythread->stack, mythread->stack_size);
     }
-    free(mythread);
+    free(mythread); 
     return 0;
 }
