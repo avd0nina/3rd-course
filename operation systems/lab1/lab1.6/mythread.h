2bcab5f132833689149dfe7e269e153389a398a7
Avdonina Lada <144753760+avd0nina@users.noreply.github.com>
2025-10-16 01:10:16 +0700

    Update mythread.h

--- operation systems/lab1/lab1.6/mythread.h
+++ operation systems/lab1/lab1.6/mythread.h
@@ -17,7 +17,21 @@ typedef struct {
 
 typedef mythread_struct_t *mythread_t;
 
+/**
+ * Создаёт новый поток, используя переданную функцию и аргумент.
+ * @param thread Указатель для хранения дескриптора потока
+ * @param start_routine Функция, выполняемая в потоке
+ * @param arg Аргумент для функции потока
+ * @return 0 при успехе, -1 при ошибке
+ */
 int mythread_create(mythread_t *thread, start_routine_t start_routine, void *arg);
+
+/**
+ * Ожидает завершения потока и освобождает его ресурсы.
+ * @param thread Дескриптор потока
+ * @param retval Указатель для возвращаемого значения (может быть NULL)
+ * @return 0 при успехе, -1 при ошибке
+ */
 int mythread_join(mythread_t thread, void **retval);
 
 #endif // MYTHREAD_H
