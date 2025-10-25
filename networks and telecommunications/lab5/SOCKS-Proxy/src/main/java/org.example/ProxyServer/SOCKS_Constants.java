908720cb62b81b81f8b1ad7771af8aadb2baef2e
Avdonina Lada <144753760+avd0nina@users.noreply.github.com>
2025-10-25 12:27:12 +0700

    Create SOCKS_Constants.java

---
+++ networks and telecommunications/lab5/SOCKS-Proxy/src/main/java/org.example/ProxyServer/SOCKS_Constants.java
@@ -0,0 +1,25 @@
+package org.example.ProxyServer;
+
+public class SOCKS_Constants {
+    static final byte SOCKS_5 = 0x05;
+    static final byte NO_AUTH_REQUIRED = 0x00;
+    // static final byte USERNAME_PASSWORD = 0x02;
+    static final byte IPV4 = 0x01;
+    static final byte DOMAIN_NAME = 0x03;
+    static final byte IPV6 = 0x04;
+    static final byte RSV = 0x00;
+    static final byte CONNECT = 0x01;
+    // static final byte BIND = 0x02;
+    static final byte SUCCESS = 0x00;
+    static final byte SERVER_FAIL = 0x01;
+    static final byte CONNECTION_NOT_ALLOWED = 0x02;
+    // static final byte NETWORK_UNREACHABLE = 0x03;
+    static final byte HOST_UNREACHABLE = 0x04;
+    // static final byte CONNECTION_REFUSED = 0x05;
+    // static final byte TTL_EXPIRED = 0x06;
+    static final byte COMMAND_NOT_SUP = 0x07;
+    static final byte ADDR_TYPE_NOT_SUP = 0x08;
+    static final byte NO_ACCEPT_METHOD = (byte) 0xFF;
+    static final int IPV4_LENGTH = 4;
+    static final int IPV6_LENGTH = 16;
+}
