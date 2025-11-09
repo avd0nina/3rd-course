6c573bf314a30d6426e710bf4bfe2d13254b85d5
Avdonina Lada <144753760+avd0nina@users.noreply.github.com>
2025-11-09 18:55:12 +0700

    Update spinlock.h

--- operation systems/lab2/lab2.4/spinlock/spinlock.h
+++ operation systems/lab2/lab2.4/spinlock/spinlock.h
@@ -1,13 +1,32 @@
 #ifndef SPINLOCK_H
 #define SPINLOCK_H
 
+/**
+ * Пользовательский спинлок на основе атомарных операций.
+ */
 typedef struct {
-    volatile int lock;  
+    /** Флаг блокировки: 0 - свободен, 1 - захвачен. */
+    volatile int lock;
 } custom_spinlock_t;
 
+/**
+ * Инициализирует спинлок в свободное состояние.
+ */
 void custom_spin_init(custom_spinlock_t *lock);
+
+/**
+ * Атомарно захватывает спинлок с активным ожиданием.
+ */
 void custom_spin_lock(custom_spinlock_t *lock);
+
+/**
+ * Освобождает спинлок.
+ */
 void custom_spin_unlock(custom_spinlock_t *lock);
+
+/**
+ * Уничтожение спинлока.
+ */
 void custom_spin_destroy(custom_spinlock_t *lock);
 
 #endif
