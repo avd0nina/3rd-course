4c31881acfc29814017ab4ac0cda203db914edf9
Avdonina Lada <144753760+avd0nina@users.noreply.github.com>
2025-11-10 14:28:35 +0700

    Create cost.ts

---
+++ translation and verification methods/lab7/cost.ts
@@ -0,0 +1,14 @@
+import { Expr } from "../../lab04";
+
+export function cost(e: Expr): number {
+    switch (e.type) {
+        case "num":
+            return 0;
+        case "var":
+            return 1;
+        case "neg":
+            return 1 + cost(e.arg);
+        case "bin":
+            return 1 + cost(e.left) + cost(e.right);
+    }
+}
