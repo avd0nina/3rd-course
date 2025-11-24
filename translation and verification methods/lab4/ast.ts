5d0ddd000e067287ad834ef4cfa3b18f43672dea
Avdonina Lada <144753760+avd0nina@users.noreply.github.com>
2025-11-24 22:04:21 +0700

    Update ast.ts

--- translation and verification methods/lab4/ast.ts
+++ translation and verification methods/lab4/ast.ts
@@ -1,23 +1,23 @@
-export type Expr = Num | Var | Neg | Bin;
-
-export interface Num {
+export interface NumLiteral {
     type: 'num';
     value: number;
 }
 
-export interface Var {
+export interface Variable {
     type: 'var';
     name: string;
 }
 
-export interface Neg {
-    type: 'neg';
-    arg: Expr;
-}
-
-export interface Bin {
-    type: 'bin';
+export interface BinaryOp {
+    type: 'binary';
     op: '+' | '-' | '*' | '/';
     left: Expr;
     right: Expr;
 }
+
+export interface UnaryNeg {
+    type: 'neg';
+    argument: Expr;
+}
+
+export type Expr = NumLiteral | Variable | BinaryOp | UnaryNeg;
