b3b8c99a356f3853158fa508c5a95cd3724a2c7d
Avdonina Lada <144753760+avd0nina@users.noreply.github.com>
2025-11-24 22:03:51 +0700

    Update index.ts

--- translation and verification methods/lab3/index.ts
+++ translation and verification methods/lab3/index.ts
@@ -1,26 +1,29 @@
-import { MatchResult } from "ohm-js";
-import grammar from "./arith.ohm-bundle";
+import {  MatchResult } from "ohm-js";
+import grammar  from "./arith.ohm-bundle";
 import { arithSemantics } from "./calculate";
 
 export const arithGrammar = grammar;
-export { ArithmeticActionDict, ArithmeticSemantics } from './arith.ohm-bundle';
+export {ArithmeticActionDict, ArithmeticSemantics} from './arith.ohm-bundle';
 
-export function evaluate(content: string, params?: { [name: string]: number }): number {
+export function evaluate(content: string, params?: {[name:string]:number}): number
+{
     return calculate(parse(content), params ?? {});
 }
-
-export class SyntaxError extends Error {
+export class SyntaxError extends Error
+{
 }
 
-function parse(content: string): MatchResult {
-    const match = grammar.match(content, "Expr");
+export function parse(content: string): MatchResult
+{
+    const match = grammar.match(content);
     if (match.failed()) {
-        const msg = (match as any).message ?? "Syntax error while parsing expression.";
-        throw new SyntaxError(msg);
+        throw new SyntaxError(match.message);
     }
     return match;
 }
 
-function calculate(expression: MatchResult, params: { [name: string]: number }): number {
-    return arithSemantics(expression).calculate(params);
+function calculate(expression: MatchResult, params: {[name:string]: number}): number
+{
+    const result = arithSemantics(expression).calculate(params);
+    return result;
 }
