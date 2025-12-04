42bd0f6a32e3949d82f9ccb1de6931ddc8452356
Avdonina Lada <144753760+avd0nina@users.noreply.github.com>
2025-12-04 21:33:14 +0700

    Update log.h

--- operation systems/lab3/CACHE_PROXY/include/log.h
+++ operation systems/lab3/CACHE_PROXY/include/log.h
@@ -1,6 +1,12 @@
 #ifndef CACHE_PROXY_LOG_H
 #define CACHE_PROXY_LOG_H
 
+/**
+ * @brief Выводит лог-сообщение с форматированием в стандартный поток ошибок (stderr)
+ *      с добавлением временной метки и префикса.
+ * @param format Строка формата в стиле printf
+ * @param ... Аргументы для подстановки в строку формата
+ */
 void proxy_log(const char* format, ...);
 
 #endif // CACHE_PROXY_LOG_H
