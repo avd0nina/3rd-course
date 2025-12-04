ba5cc542a1eb5731d230c49c2f5207d1018f50fe
Avdonina Lada <144753760+avd0nina@users.noreply.github.com>
2025-12-04 21:32:56 +0700

    Update env.h

--- operation systems/lab3/CACHE_PROXY/include/env.h
+++ operation systems/lab3/CACHE_PROXY/include/env.h
@@ -3,7 +3,18 @@
 
 #include <time.h>
 
+/**
+ * @brief Получает количество обработчиков клиентов из переменных окружения
+ * @details Читает значение из переменной окружения CLIENT_HANDLER_COUNT
+ * @return Количество обработчиков клиентов (по умолчанию 10)
+ */
 int env_get_client_handler_count();
+
+/**
+ * @brief Получает время жизни кэша из переменных окружения
+ * @details Читает значение из переменной окружения CACHE_EXPIRED_TIME_MS
+ * @return Время жизни элемента кэша в миллисекундах (по умолчанию 60000)
+ */
 time_t env_get_cache_expired_time_ms();
 
 #endif // CACHE_PROXY_ENV_H
