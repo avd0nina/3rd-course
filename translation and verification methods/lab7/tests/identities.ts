5e29ae7032b5ce55ec3697df604017c996459892
Avdonina Lada <144753760+avd0nina@users.noreply.github.com>
2025-12-17 21:19:09 +0700

    Create identities.ts

---
+++ translation and verification methods/lab7/tests/identities.ts
@@ -0,0 +1,42 @@
+import { Expr, parseExpr } from "../../lab04/out";
+
+const parseIdentities = (identities: [string, string][]): [Expr, Expr][] => identities.map(([l, r]) => [parseExpr(l), parseExpr(r)]);
+export const basicIdentities = () => parseIdentities([
+    ["x+0", "x"],
+    ["0+x", "x"],
+    ["x*0", "0"],
+    ["0*x", "0"],
+    ["x-0", "x"],
+    ["x-x", "0"],
+    ["x*1", "x"],
+    ["1*x", "x"],
+    ["x+x", "2*x"],
+    ["x/1", "x"],
+    ["x--y", "x+y"]
+]);
+
+export const commutativeIdentities = () => [
+    ...basicIdentities(), 
+    ...parseIdentities([
+        ["x+y", "y+x"],
+        ["x*y", "y*x"],
+        ["x-y", "x+-y"],
+        ["--x", "x"]
+    ])];
+
+export const toughIdentities = () => [
+    ...commutativeIdentities(),
+    ...distributiveIdentities(),
+    ...regroupIdentities()
+]
+
+const distributiveIdentities = () => parseIdentities([
+    ["a*(b+c)", "a*b + a*c"],
+    ["(b+c)*a", "b*a + c*a"],
+    ["a*(b-c)", "a*b - a*c"],
+    ["(b-c)*a", "b*a - c*a"],
+]);
+const regroupIdentities = () => parseIdentities([
+    ["a+(b+c)", "b+(c+a)"],
+    ["a+(b-c)", "b+(-c+a)"],
+]);
