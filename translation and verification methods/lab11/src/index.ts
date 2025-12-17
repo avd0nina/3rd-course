b89d8a0c80f2106dfb3de6df5e17f5e872d20f3a
Avdonina Lada <144753760+avd0nina@users.noreply.github.com>
2025-12-17 21:36:50 +0700

    Create index.ts

---
+++ translation and verification methods/lab11/src/index.ts
@@ -0,0 +1,11 @@
+import { ExportWrapper, compileModule } from "../../lab09";
+import { parseFunnier } from "../../lab10";
+import { verifyModule } from "./verifier";
+
+export async function parseVerifyAndCompile(name: string, source: string): Promise<Record<string, Function>>
+{
+    const ast = parseFunnier(source, name);
+    await verifyModule(ast);
+    const mod = await compileModule(ast);
+    return new ExportWrapper(mod);
+}
