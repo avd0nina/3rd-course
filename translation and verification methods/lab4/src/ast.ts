188903e67d98451d1270c9dff27a043ba6b3c5ea
Avdonina Lada <144753760+avd0nina@users.noreply.github.com>
2025-12-17 21:08:37 +0700

    Create ast.ts

---
+++ translation and verification methods/lab4/src/ast.ts
@@ -0,0 +1,25 @@
+// AST node types for arithmetic expressions
+
+export interface NumLiteral {
+    type: 'num';
+    value: number;
+}
+
+export interface Variable {
+    type: 'var';
+    name: string;
+}
+
+export interface BinaryOp {
+    type: 'binary';
+    op: '+' | '-' | '*' | '/';
+    left: Expr;
+    right: Expr;
+}
+
+export interface UnaryNeg {
+    type: 'neg';
+    argument: Expr;
+}
+
+export type Expr = NumLiteral | Variable | BinaryOp | UnaryNeg;
