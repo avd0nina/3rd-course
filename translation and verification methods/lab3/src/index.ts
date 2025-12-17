c1845d94b4dc8a51bf595d8571de2ab2e32ee34a
Avdonina Lada <144753760+avd0nina@users.noreply.github.com>
2025-12-17 21:05:13 +0700

    Create index.ts

---
+++ translation and verification methods/lab3/src/index.ts
@@ -0,0 +1,29 @@
+import {  MatchResult } from "ohm-js";
+import grammar  from "./arith.ohm-bundle";
+import { arithSemantics } from "./calculate";
+
+export const arithGrammar = grammar;
+export {ArithmeticActionDict, ArithmeticSemantics} from './arith.ohm-bundle';
+
+export function evaluate(content: string, params?: {[name:string]:number}): number
+{
+    return calculate(parse(content), params ?? {});
+}
+export class SyntaxError extends Error
+{
+}
+
+export function parse(content: string): MatchResult
+{
+    const match = grammar.match(content);
+    if (match.failed()) {
+        throw new SyntaxError(match.message);
+    }
+    return match;
+}
+
+function calculate(expression: MatchResult, params: {[name:string]: number}): number
+{
+    const result = arithSemantics(expression).calculate(params);
+    return result;
+}
