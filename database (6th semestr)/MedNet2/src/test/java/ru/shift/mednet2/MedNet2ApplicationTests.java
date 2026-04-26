473146f5b1dc1bc7bdc91a4aa31f90a648c323a1
Avdonina Lada <144753760+avd0nina@users.noreply.github.com>
2026-04-26 15:38:08 +0700

    Add files via upload

---
+++ database (6th semestr)/MedNet2/src/test/java/ru/shift/mednet2/MedNet2ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.shift.mednet2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedNet2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
