53a1a0d19569e9b91b645482c0632de0c189775c
Avdonina Lada <144753760+avd0nina@users.noreply.github.com>
2026-04-26 15:24:20 +0700

    Add files via upload

---
+++ database (6th semestr)/MedNet2/src/main/java/ru/shift/mednet2/MedNet2Application.java
@@ -0,0 +1,13 @@
+package ru.shift.mednet2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedNet2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MedNet2Application.class, args);
+    }
+
+}
